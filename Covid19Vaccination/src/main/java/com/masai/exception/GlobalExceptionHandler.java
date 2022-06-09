00ce16c33dcf7af3cdfd1e49983fd8037ed9b3c7
Chandan Kumar <99543778+RavSinghChandan@users.noreply.github.com>
2022-06-09 12:53:19 +0530

    MehtodNotFound
    Print the message

--- Covid19Vaccination/src/main/java/com/masai/exception/GlobalExceptionHandler.java
+++ Covid19Vaccination/src/main/java/com/masai/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -116,7 +117,11 @@ public class GlobalExceptionHandler {
 				
 	}
 	
-
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<MyErrorDetails> myMANVExceptionHandler(MethodArgumentNotValidException me) {
+	MyErrorDetails err=new MyErrorDetails(LocalDateTime.now(),"Validation Error",me.getBindingResult().getFieldError().getDefaultMessage());
+	return new ResponseEntity<>(err,HttpStatus.BAD_REQUEST);
+	}
 
 	
 	
